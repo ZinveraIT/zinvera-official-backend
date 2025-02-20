53f6cf493a907560cd4042259bd6b08cc720e633
Roton02 <rph645102@gmail.com>
2025-02-20 20:59:33 +0600

    use multer and cloudinary for secure image hosting and getting url

--- src/app/modules/auth/user.routes.ts
+++ src/app/modules/auth/user.routes.ts
@@ -4,11 +4,15 @@ import zodValidator from '../../middleware/validator'
 import { authValidation } from './user.validation'
 import auth from '../../middleware/auth'
 
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' })
+
 const userRouter = Router()
 
 userRouter.post(
   '/auth/register',
   zodValidator(authValidation.userValidationSchema),
+  upload.single('picture'),
   userControlloer.createUser
 )
 userRouter.post(
--- src/app/modules/auth/user.validation.ts
+++ src/app/modules/auth/user.validation.ts
@@ -1,21 +1,23 @@
 import { z } from 'zod'
 
 const userValidationSchema = z.object({
-  userName: z.string({
-    required_error: 'UserName is required',
+  data: z.object({
+    userName: z.string({
+      required_error: 'UserName is required',
+    }),
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email('Invalid email address'),
+    picture: z.string().optional(), // Optional field
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+    role: z.string().default('user'),
+    isBlocked: z.boolean().default(false),
+    isDeleted: z.boolean().default(false),
   }),
-  email: z
-    .string({
-      required_error: 'Email is required',
-    })
-    .email('Invalid email address'),
-  picture: z.string().optional(), // Optional field
-  password: z.string({
-    required_error: 'Password is required',
-  }),
-  role: z.string().default('user'),
-  isBlocked: z.boolean().default(false),
-  isDeleted: z.boolean().default(false),
 })
 
 const loginUserVaidation = z.object({
