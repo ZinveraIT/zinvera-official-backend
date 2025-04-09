a291c4aaeb230c6f66ec25b4e22a4a41d682606b
Roton02 <rph645102@gmail.com>
2025-04-09 23:31:43 +0600

    complete backend project

--- src/types/email-verfication.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'email-verifier' {
-  export default class Verifier {
-    constructor(apiKey: string)
-    verify(email: string, callback: (err: any, data: any) => void): void
-  }
-}
