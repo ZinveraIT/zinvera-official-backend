dbe400587d53e76a9bfcfecad17772dbca4775f2
Roton02 <rph645102@gmail.com>
2025-04-10 23:54:52 +0600

    refactor the code

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function server() {
     await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port} 🔥`)
+      console.log(`Server running on port ${config.port} `)
     })
   } catch (error) {
     console.error(error)
